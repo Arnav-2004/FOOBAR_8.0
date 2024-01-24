91615dbfe2b2bfdf18f9b035ffcb73def22b6231
Arnav-2004 <arnavrangwani@gmail.com>
2024-01-24 17:06:57 +0530

    Initial Commit

---
+++ assets/js/eventList.js
@@ -0,0 +1,354 @@
+const eventList = {
+  "six": [
+    {
+      "id": "openmic",
+      "name": "OPEN MIC",
+      "time": "1:00 PM",
+      "venue": "Open Audi",
+      "register": "https://forms.gle/Zdng2YqDUob2vYh28",
+      "more": "event.html?event=openmic"
+    }
+  ],
+  "seven": [
+    {
+      "id": "ai",
+      "name": "AI HACKATHON",
+      "time": "2:00 PM",
+      "venue": "242, Block II",
+      "register": "https://forms.gle/KAp4ERDtBzBLaXwL6",
+      "more": "event.html?event=aihackathon"
+    }
+  ],
+  "eight": [
+    {
+      "id": "query",
+      "name": "BREAK THE QUERY",
+      "time": "Postponed",
+      "venue": "257, Block II",
+      "register": "https://forms.gle/4uipUXZzcTAyomL47",
+      "more": "event.html?event=breakthequery"
+    }
+  ],
+  "nine": [
+    {
+      "id": "codeathon",
+      "name": "CODEATHON",
+      "time": "4:00 PM",
+      "venue": "255, Block II",
+      "register": "https://forms.gle/hRJEKMbe4sBo5XjMA",
+      "more": "event.html?event=codeathon"
+    }
+  ],
+  "ten": [
+    {
+      "id": "webally",
+      "name": "WEB ALLY",
+      "time": "1:00 PM",
+      "venue": "246, Block II",
+      "register": "https://forms.gle/oMhrSXaodaFe6EYF9",
+      "more": "event.html?event=webally"
+    },
+    {
+      "id": "illustra",
+      "name": "ILLUSTRA",
+      "time": "2:00 PM",
+      "venue": "242, Block II",
+      "register": "https://forms.gle/Q8ioT35J7Aybjbpx7",
+      "more": "event.html?event=illustra"
+    }
+  ],
+  "eleven": [
+    {
+      "id": "ace",
+      "name": "ACE CLUTCH",
+      "time": "10:00 AM",
+      "venue": "242, Block II",
+      "register": "https://forms.gle/whGnfCmXXMc3eFPw7",
+      "more": "event.html?event=aceclutch"
+    }
+  ],
+  "tweleve": [
+    {
+      "id": "nothing",
+      "type": "noevent"
+    }
+  ],
+  "thirteen": [
+    {
+      "id": "image",
+      "name": "IMAGE BRUSH",
+      "time": "4:00 PM",
+      "venue": "242, Block II",
+      "register": "https://forms.gle/MdH5oZED5BVrstwg8",
+      "more": "event.html?event=imagebrush"
+    },
+    {
+      "id": "query",
+      "name": "BREAK THE QUERY",
+      "time": "4:00PM",
+      "venue": "257, Block II",
+      "register": "https://forms.gle/4uipUXZzcTAyomL47",
+      "more": "event.html?event=breakthequery"
+    }
+  ],
+  "fourteen": [
+    {
+      "id": "short",
+      "name": "KB GAMES",
+      "time": "2:00 PM",
+      "venue": "242, Block II",
+      "register": "https://forms.gle/JdbweAN2XhjreXou8",
+      "more": "event.html?event=gamedevelopment"
+    },
+    {
+      "id": "flix",
+      "name": "FLIX n MANGA",
+      "time": "4:00 PM",
+      "venue": "256, Block II",
+      "register": "https://forms.gle/KJF9j3QGJLUcU4v5A",
+      "more": "event.html?event=flixnmanga"
+    }
+  ],
+  "fifteen": [
+    {
+      "id": "blind",
+      "name": "BLIND TYPING",
+      "time": "4:00 PM",
+      "venue": "257, Block II",
+      "register": "https://forms.gle/kpkvRDHMNGVejeUi7",
+      "more": "event.html?event=blindtyping"
+    }
+  ],
+  "sixteen": [
+    {
+      "id": "css",
+      "name": "CSS BATTLE",
+      "time": "4:00 PM",
+      "venue": "215, Block II",
+      "register": "https://forms.gle/fTD8DzgAsz9d6RyZ8",
+      "more": "event.html?event=cssbattle"
+    }
+  ],
+  "seventeen": [
+    {
+      "id": "quiz",
+      "name": "TECH QUIZ",
+      "time": "2:00 PM",
+      "venue": "Audi, Block I",
+      "register": "https://forms.gle/GW4jrHXvGkgaXngD6",
+      "more": "event.html?event=techquiz"
+    }
+  ],
+  "eighteen": [
+    {
+      "id": "nothing",
+      "type": "noevent"
+    }
+  ],
+  "nineteen": [
+    {
+      "id": "nothing",
+      "type": "noevent"
+    }
+  ],
+  "twenty": [
+    {
+      "id": "accio",
+      "name": "ACCIO LA NOCIEN",
+      "time": "4:00 PM",
+      "venue": "242, Block II",
+      "register": "https://forms.gle/5Us54QTy62CMNCUd6",
+      "more": "event.html?event=acciolanocien"
+    }
+  ],
+  "twentyone": [
+    {
+      "id": "gameworkshop",
+      "name": "GAMING WORKSHOP - Day 1",
+      "time": "2:00 PM",
+      "venue": "245, Block II",
+      "register": "https://forms.gle/LvhatwCkN87r6E57A",
+      "more": "event.html?event=gamedevworkshop"
+    },
+    {
+      "id": "ideathon",
+      "name": "IDEATHON",
+      "time": "4:00 PM",
+      "venue": "255, Block II",
+      "register": "https://forms.gle/wJP7ZkktAWFFLyZ36",
+      "more": "event.html?event=ideathon"
+    }
+  ],
+  "twentytwo": [
+    {
+      "id": "gameworkshop",
+      "name": "GAMING WORKSHOP - Day 2",
+      "time": "4:00 PM",
+      "venue": "245, Block II",
+      "register": "https://forms.gle/LvhatwCkN87r6E57A",
+      "more": "event.html?event=gamedevworkshop"
+    },
+    {
+      "id": "java",
+      "name": "JAVA WORKSHOP - Day 1",
+      "time": "4:00 PM",
+      "venue": "242, Block II",
+      "register": "https://forms.gle/uFsiMC1qX1x9rRGTA",
+      "more": "event.html?event=javaworkshop"
+    }
+  ],
+  "twentythree": [
+    {
+      "id": "java",
+      "name": "JAVA WORKSHOP - Day 2",
+      "time": "4:00 PM",
+      "venue": "242, Block II",
+      "register": "https://forms.gle/uFsiMC1qX1x9rRGTA",
+      "more": "event.html?event=javaworkshop"
+    }
+  ],
+  "twentyfour": [
+    {
+      "id": "c",
+      "name": "C WORKSHOP - Day 1",
+      "time": "2:00 PM",
+      "venue": "242, Block II",
+      "register": "https://forms.gle/qwNr9AveevnpGuuMA",
+      "more": "event.html?event=cworkshop"
+    },
+    {
+      "id": "researchworkshop",
+      "name": "RESEARCH WRITING WORKSHOP",
+      "time": "9:00 AM",
+      "venue": "Audi, Block I",
+      "register": "https://forms.gle/jrn7KFAZAGH58PXP6",
+      "more": "event.html?event=researchworkshop"
+    }
+  ],
+  "twentyfive": [
+    {
+      "id": "c",
+      "name": "C WORKSHOP - Day 2",
+      "time": "2:00 PM",
+      "venue": "242, Block II",
+      "register": "https://forms.gle/qwNr9AveevnpGuuMA",
+      "more": "event.html?event=cworkshop"
+    }
+  ],
+  "twentysix": [
+    {
+      "id": "nothing",
+      "type": "noevent"
+    }
+  ],
+  "twentyseven": [
+    {
+      "id": "web",
+      "name": "WEB DEVELOPMENT WORKSHOP - Day 1",
+      "time": "4:00 PM",
+      "venue": "242, Block II",
+      "register": "https://forms.gle/xRLqaT32jDSkiMyo7",
+      "more": "event.html?event=webdevworkshop"
+    }
+  ],
+  "twentyeight": [
+    {
+      "id": "web",
+      "name": "WEB DEVELOPMENT WORKSHOP - Day 2",
+      "time": "2:00 PM",
+      "venue": "242, Block II",
+      "register": "https://forms.gle/xRLqaT32jDSkiMyo7",
+      "more": "event.html?event=webdevworkshop"
+    }
+  ]
+}
+
+
+for (const [key, value] of Object.entries(eventList)) {
+  const mid = document.createElement("div");
+  mid.setAttribute('class', `events-container ${key}`)
+  const midspan = document.createElement("span");
+  midspan.setAttribute('class', 'events__title')
+
+  const elem = document.createElement("ul");
+  elem.setAttribute('class', 'events__list')
+  value.map(event => {
+    var child = document.createElement("li");
+    child.setAttribute('class', 'events__item')
+
+    if ((event.type != undefined && event.type == "noevent") || event.id == "inauguration"){
+      const h3h3 = document.createElement('h3');
+      if (event.id == "inauguration") {
+        h3h3.setAttribute('class', 'noevents__title blackclr')
+        h3h3.innerText = "Inauguration";
+      child.appendChild(h3h3);
+      } else {
+        h3h3.setAttribute('class', 'noevents__title')
+        h3h3.innerText = "No events found";
+        child.appendChild(h3h3);
+      }
+      
+
+    } else {
+
+      var spanname = document.createElement("span");
+      spanname.setAttribute('class', 'events__name')
+      spanname.innerText = event.name;
+
+      var spantag = document.createElement("span");
+      spantag.setAttribute('class', 'events__tag')
+      spantag.innerText = event.time;
+
+      var divtag = document.createElement("div");
+      divtag.setAttribute('class', 'events__item--left')
+
+      divtag.appendChild(spanname);
+      divtag.appendChild(spantag);
+
+
+
+      //
+      var atag1 = document.createElement("a");
+      atag1.classList.add("atagwhite");
+      atag1.setAttribute('href', event.register)
+      atag1.innerText = "Register"
+
+      var atag2 = document.createElement("a");
+      atag2.classList.add("atagwhite");
+      atag2.setAttribute('href', event.more)
+      atag2.innerText = "More Info"
+
+      var spantag2a = document.createElement("div");
+      spantag2a.setAttribute('class', 'events__tag regbtn')
+      spantag2a.setAttribute('onclick', `window.location.href="${event.register}"`)
+      spantag2a.appendChild(atag1);
+
+      var spantag2b = document.createElement("div");
+      spantag2b.setAttribute('class', 'events__tag regbtn')
+      spantag2b.setAttribute('onclick', `window.location.href="${event.more}"`)
+      spantag2b.appendChild(atag2);
+
+      var divtag2 = document.createElement("div");
+      divtag2.setAttribute('class', 'dispflex')
+      divtag2.appendChild(spantag2a)
+      divtag2.appendChild(spantag2b)
+
+      child.appendChild(divtag);
+      child.appendChild(divtag2);
+    }
+    elem.appendChild(child);
+  })
+
+  midspan.innerText = "Upcoming events";
+  mid.appendChild(midspan)
+  mid.appendChild(elem);
+  document.getElementsByClassName('portfolio-container')[0].appendChild(mid)
+}
+
+var numDate = new Date().getDate()
+
+setTimeout(() => {
+  var numDate = new Date().getDate()
+  document.getElementById(`calenderid${numDate}`).click()
+}, 1000);
+
